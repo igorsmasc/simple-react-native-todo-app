f8563371fca4530f1bc46a36f3201a668f9986ff
Igor Santos Mascarenhas <46729174+igorsmasc@users.noreply.github.com>
2020-06-18 12:51:02 -0300

    Add alert

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Alert } from 'react-native';
 import Header from './components/header';
 import TodoItem from './components/todoItem';
 import AddTodo from './components/addTodo'
@@ -18,11 +18,19 @@ export default function App() {
   }
 
   const submitHandler = (text) => {
-    setTodos((prevTodos) => {
-      return [
-        { text: text, key: Math.random().toString() },
-        ...prevTodos, ]
-    })
+
+    if(text.length > 3) {
+      setTodos((prevTodos) => {
+        return [
+          { text: text, key: Math.random().toString() },
+          ...prevTodos, ]
+      })
+    } else {
+      Alert.alert('OOPS!', 'To do must be over 3 chars long', [
+        {text: 'Undestood', onPress: () => console.log('Alert closed!')}
+      ]);
+    }
+    
   }
 
 
