13877ad2feb6e10e7e419d9bd5ea8b32f3d253cf
Igor Santos Mascarenhas <46729174+igorsmasc@users.noreply.github.com>
2020-06-18 00:50:15 -0300

    Ajust app.js and add header file

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Header from './components/header';
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { text: 'buy coffee', key: '1' },
+    { text: 'create an app', key: '2' },
+    { text: 'play on the switch', key: '3' }
+  ]);
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Header />
+      <View style={styles.content}>
+        {/** to do form */}
+        <View style={styles.list}>
+          <FlatList 
+          data={todos}
+          renderItem={({ item }) => (
+            <Text>{ item.text }</Text>
+          )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,7 +32,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  }, 
+  content: {
+    padding: 40,
   },
+  list: {
+    marginTop: 20
+  }
 });
